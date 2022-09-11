bdeeb30d9381eb28cce4646ffa9f4afac47910ca
Zahed75 <Zahedhasan.CS@gmail.com>
2022-09-12 03:40:23 +0600

    3 funtion done

--- main_app/views.py
+++ main_app/views.py
@@ -10,3 +10,25 @@ def index(request):
    
     dict = {'welcontent':welcontent,'course_info':course_info}
     return render(request, 'main_app/index.html', context=dict)
+
+
+
+
+def about(request):
+    dict={}
+
+    return render(request,'main_app/about.html',context=dict)
+
+
+
+def course(request):
+    dict={}
+
+    return render(request,'main_app/course.html',context=dict)
+
+
+
+def teacher(request):
+    dict={}
+
+    return render(request,'main_app/teachers.html',context=dict)
