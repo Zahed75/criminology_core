d4f10bf65519a3e12345e0388b175b51f6dd5f84
Zahed75 <Zahedhasan.CS@gmail.com>
2022-09-13 05:42:53 +0600

    server updated

--- criminology_core/settings.py
+++ criminology_core/settings.py
@@ -25,9 +25,9 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = 'django-insecure-mi%v4kpg@921@-!28r1546l$=gn%%p*47_+()p-c_c2km(4bvl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-PRODUCTION = False
+PRODUCTION = True
 
 ALLOWED_HOSTS = ['*']
 
