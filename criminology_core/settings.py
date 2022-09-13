217aa593551aaa1beb888ed3e9aa817a0309bf8f
Zahed75 <Zahedhasan.CS@gmail.com>
2022-09-13 17:15:06 +0600

    data up

--- criminology_core/settings.py
+++ criminology_core/settings.py
@@ -141,3 +141,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'public/static/')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
+# MEDIA_DIR
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
