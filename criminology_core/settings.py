c70e5c59ff8e68891d98c69ea3f0cc5a0c1d2998
Zahed75 <Zahedhasan.CS@gmail.com>
2022-09-16 00:24:01 +0600

    data up

--- criminology_core/settings.py
+++ criminology_core/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 PRODUCTION = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['www.criminology.du.ac.bd']
 # ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 # Application definition
